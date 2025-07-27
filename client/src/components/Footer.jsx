9d9222761fd3c2f857bd0821ffcc03f58af56bbf
Vicky Kumar <vickykumarchainpur@gmail.com>
2025-07-27 16:24:46 +0530

    Updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                     <p className=' font-playfair text-lg text-gray-800'>COMPANY</p>
                     <ul className='mt-3 flex flex-col gap-2 text-sm'>
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Career</a></li>
                         <li><a href="#">Press</a></li>
                         <li><a href="#">Blog</a></li>
                         <li><a href="#">Partners</a></li>
